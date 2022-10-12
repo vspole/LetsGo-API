e93c7dd942564b5b1e0498bad6fcb3ea5c309cba
vspole <vishal.polepalli@gmail.com>
2022-10-12 13:21:42 -0500

    Added firebase keys to github secrets for CD and ignore local key json

---
+++ .github/pull_request_template.md
@@ -0,0 +1,5 @@
+### What's in this PR
+_Write a short description of what this PR adds or fixes._
+
+### Known Issues
+_Are there any known issues which the team should be aware of?_
--- Firebase/firebase.py
+++ Firebase/firebase.py
@@ -4,7 +4,7 @@ from firebase_admin import firestore
 from Models.models import *
 
 
-cred = credentials.Certificate('Keys/letsgo-69e94-06d6c9bc1328.json')
+cred = credentials.Certificate('Keys/FirebaseKey.json')
 
 app = firebase_admin.initialize_app(cred)
 
