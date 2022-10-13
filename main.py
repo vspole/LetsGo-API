176d6f7456644bb497a0fb1f879bd322365c993d
Vishal Polepalli <vishal.polepalli@gmail.com>
2022-10-13 00:03:09 -0500

    Added validation for FIrebase ID Token and API Client

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException, status
 from Models.models import *
 from Firebase.firebase import *
+from secrets import *
+import json
+
+
 
 app = FastAPI()
-
+apiKey = getAPIKey()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the API of Let's Go! Explore!"}
 
 @app.post("/createUser", response_model = User )
-async def createUser(user: User):
+async def createUser(user: User, userToken: str = Header(...), clientKey: str =  Header(...)):
+    verifyAccess(userToken, clientKey)
     newUser = addUserToGroupFirebase(user)
     return newUser
+
+
+def verifyAccess(userToken: str, clientKey: str):
+    if verifyIDToken(userToken) == False:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate User ID Token")
+    if clientKey != apiKey:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate API Client Key")
